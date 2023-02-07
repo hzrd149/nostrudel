92ccf31d3205173817a22a7e6d5d8f8a737bb5d5
hzrd149 <github@hzrd149.com>
2023-02-07 17:04:18 -0600

    add spotify and tidal embeds fix bug with multiple embeds

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 - [x] Dark theme
 - [x] Preview twitter / youtube links
 - [x] Lighting invoices
+- [x] Blurred or hidden images and embeds for people you dont follow
 - [ ] Profile management
 - [ ] Make post
 - [ ] Relay management
@@ -17,8 +18,8 @@
 - [ ] Thread view
 - [ ] User tipping
 - [ ] Reactions
-- [ ] Blurred or hidden images and embeds for people you dont follow
 - [ ] "Read more" Button for long posts
+- [ ] Dynamically connect to relays (start with one relay then connect to others as required)
 
 ## Setup
 
--- src/components/post/post-contents.tsx
+++ src/components/post/post-contents.tsx
@@ -38,9 +38,37 @@ const embeds: { regexp: RegExp; render: (match: RegExpMatchArray, trusted: boole
       </AspectRatio>
     ),
   },
+  // Tidal
+  {
+    regexp: /https?:\/\/tidal\.com\/track\/(\d+)/im,
+    render: (match) => (
+      <iframe
+        src={`https://embed.tidal.com/tracks/${match[1]}?disableAnalytics=true`}
+        width="100%"
+        height="96"
+      ></iframe>
+    ),
+  },
+  // Spotify
+  {
+    regexp: /https?:\/\/open\.spotify\.com\/track\/(\w+)[^\s]+/im,
+    render: (match) => (
+      <iframe
+        style={{ borderRadius: "12px" }}
+        width="100%"
+        height="152"
+        title="Spotify Embed: Beethoven - Fur Elise - Komuz Remix"
+        frameBorder="0"
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+        src={`https://open.spotify.com/embed/track/${match[1]}`}
+      ></iframe>
+    ),
+  },
   // Image
   {
-    regexp: /(https?:\/\/)([\da-z\.-]+\.[a-z\.]{2,6})([\/\w\.-]+\.(svg|gif|png|jpg|jpeg|webp|avif))/im,
+    regexp: /(https?:\/\/)([\da-z\.-]+\.[a-z\.]{2,6})([\/\w\.-]+\.(svg|gif|png|jpg|jpeg|webp|avif))[^\s]*/im,
     render: (match, trusted) => {
       const ImageComponent = trusted ? Image : BlurredImage;
       return <ImageComponent key={match[0]} src={match[0]} maxWidth="30rem" />;
@@ -48,7 +76,7 @@ const embeds: { regexp: RegExp; render: (match: RegExpMatchArray, trusted: boole
   },
   // Video
   {
-    regexp: /(https?:\/\/)([\da-z\.-]+\.[a-z\.]{2,6})([\/\w\.-]+\.(mp4|mkv|webm|mov))/im,
+    regexp: /(https?:\/\/)([\da-z\.-]+\.[a-z\.]{2,6})([\/\w\.-]+\.(mp4|mkv|webm|mov))[^\s]*/im,
     render: (match) => (
       <AspectRatio ratio={16 / 9} maxWidth="30rem">
         <video key={match[0]} src={match[0]} controls />
@@ -79,35 +107,26 @@ const embeds: { regexp: RegExp; render: (match: RegExpMatchArray, trusted: boole
   },
 ];
 
+function embedContent(content: string, trusted: boolean): (string | JSX.Element)[] {
+  for (const { regexp, render } of embeds) {
+    const match = content.match(regexp);
+
+    if (match && match.index !== undefined) {
+      const before = content.slice(0, match.index);
+      const after = content.slice(match.index + match[0].length, content.length);
+      return [...embedContent(before, trusted), render(match, trusted ?? false), ...embedContent(after, trusted)];
+    }
+  }
+  return [content];
+}
+
 export type PostContentsProps = {
   content: string;
   trusted?: boolean;
 };
 
 export const PostContents = React.memo(({ content, trusted }: PostContentsProps) => {
-  const parts: (string | JSX.Element)[] = [content];
-
-  for (const { regexp, render } of embeds) {
-    let i = 0;
-    while (i < 1000) {
-      i++;
-      const str = parts.pop();
-      if (typeof str !== "string" || str.length === 0) {
-        str && parts.push(str);
-        break;
-      }
-
-      const match = str.match(regexp);
-      if (match && match.index !== undefined) {
-        parts.push(str.slice(0, match.index));
-        parts.push(render(match, trusted ?? false));
-        parts.push(str.slice(match.index + match[0].length, str.length));
-      } else {
-        parts.push(str);
-        break;
-      }
-    }
-  }
+  const parts = embedContent(content, trusted ?? false);
 
   return (
     <Box whiteSpace="pre-wrap">{parts.map((part) => (typeof part === "string" ? <span>{part}</span> : part))}</Box>
