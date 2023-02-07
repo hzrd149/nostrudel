2822f992fbb05255cd3b322782943ef272b3cf05
hzrd149 <github@hzrd149.com>
2023-02-07 17:04:18 -0600

    set share enctype

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
         share_target: {
           action: "/share",
           method: "GET",
+          enctype: "application/x-www-form-urlencoded",
           params: {
             title: "title",
             text: "text",
