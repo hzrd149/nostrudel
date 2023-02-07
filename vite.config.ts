8caf80f899cfc487e5516ca9f0b3be86bf384acd
hzrd149 <github@hzrd149.com>
2023-02-07 17:04:18 -0600

    make debugging easier

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   build: {
     target: ["chrome89", "edge89", "firefox89", "safari15"],
+    minify: false,
   },
   plugins: [react()],
 });
