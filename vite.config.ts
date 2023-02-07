f2f8c1629722c99578c87b13d137717352e151e1
hzrd149 <github@hzrd149.com>
2023-02-07 17:04:18 -0600

    add offline cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     target: ["chrome89", "edge89", "firefox89", "safari15"],
   },
-  plugins: [react()],
+  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
 });
