850e781deebda54feef580203b2e8c8b5bd1a963
hzrd149 <github@hzrd149.com>
2023-03-21 09:40:41 -0600

    add reload notification to login screen

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Box, Flex, Heading } from "@chakra-ui/react";
+import { Avatar, Flex, Heading } from "@chakra-ui/react";
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { ReloadPrompt } from "../../components/reload-prompt";
 import useSubject from "../../hooks/use-subject";
 import accountService from "../../services/account";
 
@@ -10,19 +11,22 @@ export default function LoginView() {
   if (current) return <Navigate to={location.state?.from ?? "/"} replace />;
 
   return (
-    <Flex
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      gap="4"
-      height="100%"
-      padding="4"
-      overflowX="hidden"
-      overflowY="auto"
-    >
-      <Avatar src="/apple-touch-icon.png" size="lg" flexShrink={0} />
-      <Heading>noStrudel</Heading>
-      <Outlet />
-    </Flex>
+    <>
+      <ReloadPrompt />
+      <Flex
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        gap="4"
+        height="100%"
+        padding="4"
+        overflowX="hidden"
+        overflowY="auto"
+      >
+        <Avatar src="/apple-touch-icon.png" size="lg" flexShrink={0} />
+        <Heading>noStrudel</Heading>
+        <Outlet />
+      </Flex>
+    </>
   );
 }
