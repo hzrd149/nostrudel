5c4a6a7780e19f170f778ca65e2e0e3755d1aa3a
hzrd149 <github@hzrd149.com>
2023-06-29 11:29:47 -0500

    organize list view

--- src/views/lists/index.tsx
+++ src/views/lists/index.tsx
@@ -1,9 +1,11 @@
-import { Box, Button, Divider, Flex } from "@chakra-ui/react";
+import { Button, Divider, Flex, Heading, Image, Link, Spacer } from "@chakra-ui/react";
 import { useCurrentAccount } from "../../hooks/use-current-account";
 import { useReadRelayUrls } from "../../hooks/use-client-relays";
 import useUserLists from "../../hooks/use-user-lists";
 import { Link as RouterLink } from "react-router-dom";
-import { PlusCircleIcon } from "../../components/icons";
+import { ExternalLinkIcon, PlusCircleIcon } from "../../components/icons";
+import { useIsMobile } from "../../hooks/use-is-mobile";
+import RequireCurrentAccount from "../../providers/require-current-account";
 
 function UsersLists() {
   const account = useCurrentAccount()!;
@@ -22,18 +24,32 @@ function UsersLists() {
   );
 }
 
-export default function ListsView() {
-  const account = useCurrentAccount();
-
+function ListsPage() {
   return (
-    <Flex direction="column" px="2" overflowY="auto" overflowX="hidden" h="full" gap="2">
-      {account && (
-        <>
-          <UsersLists />
-          <Divider />
-          <Button leftIcon={<PlusCircleIcon />}>New List</Button>
-        </>
-      )}
+    <Flex direction="column" p="2" overflowY="auto" overflowX="hidden" h="full" gap="2">
+      <Flex gap="2">
+        <Spacer />
+        <Button
+          as={Link}
+          href="https://listr.lol/"
+          isExternal
+          rightIcon={<ExternalLinkIcon />}
+          leftIcon={<Image src="https://listr.lol/favicon.ico" w="1.2em" />}
+        >
+          Listr
+        </Button>
+        <Button leftIcon={<PlusCircleIcon />}>New List</Button>
+      </Flex>
+
+      <UsersLists />
     </Flex>
+  );
+}
+
+export default function ListsView() {
+  return (
+    <RequireCurrentAccount>
+      <ListsPage />
+    </RequireCurrentAccount>
   );
 }
