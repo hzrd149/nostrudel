8985f349a93e352bf3dda362eda93ea141d626fe
hzrd149 <github@hzrd149.com>
2023-07-02 14:29:18 -0500

    add github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE ?? "/",
   build: {
     target: ["chrome89", "edge89", "firefox89", "safari15"],
   },
