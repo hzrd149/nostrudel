e4927e7d226ed662b2299110326d6c7511b6099b
hzrd149 <github@hzrd149.com>
2023-08-23 10:19:59 -0500

    fix invisible spaces breaking some hashtags

--- src/helpers/regexp.ts
+++ src/helpers/regexp.ts
@@ -5,3 +5,8 @@ export const getMatchNostrLink = () =>
 export const getMatchHashtag = () => /(^|[^\p{L}])#([\p{L}\p{N}]+)/gu;
 export const getMatchLink = () =>
   /https?:\/\/([a-zA-Z0-9\.\-]+\.[a-zA-Z]+)([\p{Letter}\p{Number}&\.-\/\?=#\-@%\+_,:]*)/gu;
+
+// read more https://www.regular-expressions.info/unicode.html#category
+export function stripInvisibleChar(str?: string) {
+  return str && str.replaceAll(/[\p{Cf}\p{Zs}]/gu, "");
+}
