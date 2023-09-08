94cd15663b2de9383c37491331bac1aaacf9f677
hzrd149 <github@hzrd149.com>
2023-09-08 12:22:21 -0500

    add share button to stream view

--- src/components/note/components/quote-repost-button.tsx
+++ src/components/note/components/quote-repost-button.tsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
-import { ButtonProps, IconButton, IconButtonProps } from "@chakra-ui/react";
+import { ButtonProps, IconButton } from "@chakra-ui/react";
 import { Kind } from "nostr-tools";
 import dayjs from "dayjs";
 
 import { NostrEvent } from "../../../types/nostr-event";
 import { QuoteRepostIcon } from "../../icons";
 import { PostModalContext } from "../../../providers/post-modal-provider";
-import { getSharableNoteId } from "../../../helpers/nip19";
+import { getSharableEventAddress } from "../../../helpers/nip19";
 
 export type QuoteRepostButtonProps = Omit<ButtonProps, "children" | "onClick"> & {
   event: NostrEvent;
@@ -14,24 +14,30 @@ export type QuoteRepostButtonProps = Omit<ButtonProps, "children" | "onClick"> &
 
 export function QuoteRepostButton({
   event,
-  "aria-label": ariaLabel = "Quote repost",
+  "aria-label": ariaLabel,
   title = "Quote repost",
   ...props
 }: QuoteRepostButtonProps) {
   const { openModal } = useContext(PostModalContext);
 
   const handleClick = () => {
-    const nevent = getSharableNoteId(event.id);
+    const nevent = getSharableEventAddress(event);
     const draft = {
       kind: Kind.Text,
       tags: [],
-      content: "nostr:" + nevent,
+      content: "\nnostr:" + nevent,
       created_at: dayjs().unix(),
     };
     openModal(draft);
   };
 
   return (
-    <IconButton icon={<QuoteRepostIcon />} onClick={handleClick} aria-label={ariaLabel} title={title} {...props} />
+    <IconButton
+      icon={<QuoteRepostIcon />}
+      onClick={handleClick}
+      aria-label={ariaLabel || title}
+      title={title}
+      {...props}
+    />
   );
 }
---
+++ src/views/streams/components/stream-share-button.tsx
@@ -0,0 +1,40 @@
+import { useContext } from "react";
+import { Button, ButtonProps } from "@chakra-ui/react";
+import { Kind } from "nostr-tools";
+import dayjs from "dayjs";
+
+import { getSharableEventAddress } from "../../../helpers/nip19";
+import { DraftNostrEvent } from "../../../types/nostr-event";
+import { PostModalContext } from "../../../providers/post-modal-provider";
+import { RepostIcon } from "../../../components/icons";
+import { ParsedStream } from "../../../helpers/nostr/stream";
+
+export type StreamShareButtonProps = Omit<ButtonProps, "children" | "onClick"> & {
+  stream: ParsedStream;
+};
+
+export default function StreamShareButton({
+  stream,
+  "aria-label": ariaLabel,
+  title = "Quote repost",
+  ...props
+}: StreamShareButtonProps) {
+  const { openModal } = useContext(PostModalContext);
+
+  const handleClick = () => {
+    const nevent = getSharableEventAddress(stream.event);
+    const draft: DraftNostrEvent = {
+      kind: Kind.Text,
+      tags: [],
+      content: "\nnostr:" + nevent,
+      created_at: dayjs().unix(),
+    };
+    openModal(draft);
+  };
+
+  return (
+    <Button leftIcon={<RepostIcon />} onClick={handleClick} aria-label={ariaLabel || title} title={title} {...props}>
+      Share
+    </Button>
+  );
+}
