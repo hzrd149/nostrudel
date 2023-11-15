e144f13e9c41f34fe6f09818dd0399aa957d7d9a
hzrd149 <github@hzrd149.com>
2023-11-15 14:41:58 -0600

    Improve how reposts and replies are displayed

---
+++ .changeset/lazy-chicken-doubt.md
@@ -0,0 +1,5 @@
+---
+"nostrudel": minor
+---
+
+Improve how reposts and replies are displayed in timelines
--- src/components/timeline-page/generic-note-timeline/reply-note.tsx
+++ src/components/timeline-page/generic-note-timeline/reply-note.tsx
@@ -1,33 +1,18 @@
 import { memo, useRef } from "react";
-import { Flex, SkeletonText, Text } from "@chakra-ui/react";
 
-import { getReferences } from "../../../helpers/nostr/events";
-import useSingleEvent from "../../../hooks/use-single-event";
 import { NostrEvent } from "../../../types/nostr-event";
-import { EmbedEvent } from "../../embed-event";
 import { useRegisterIntersectionEntity } from "../../../providers/intersection-observer";
 import Note from "../../note";
-import UserAvatar from "../../user-avatar";
-import { UserLink } from "../../user-link";
 
 function ReplyNote({ event }: { event: NostrEvent }) {
-  const refs = getReferences(event);
-  const parent = useSingleEvent(refs.replyId);
-
   // if there is a parent intersection observer, register this card
   const ref = useRef<HTMLDivElement | null>(null);
   useRegisterIntersectionEntity(ref, event.id);
 
   return (
-    <Flex gap="2" direction="column" ref={ref}>
-      <Flex gap="2">
-        <UserAvatar pubkey={event.pubkey} size="xs" alignItems="center" />
-        <UserLink pubkey={event.pubkey} />
-        <Text>Replied to:</Text>
-      </Flex>
-      {parent ? <EmbedEvent event={parent} ml="4" /> : <SkeletonText />}
-      <Note event={event} />
-    </Flex>
+    <div ref={ref}>
+      <Note event={event} showReplyButton showReplyLine />
+    </div>
   );
 }
 export default memo(ReplyNote);
