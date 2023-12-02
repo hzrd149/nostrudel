b60bf51e2c5505912c0f8d03d802b12add1f3954
hzrd149 <github@hzrd149.com>
2023-12-01 18:18:09 -0600

    small fix

--- src/components/note/components/reaction-button.tsx
+++ src/components/note/components/reaction-button.tsx
@@ -7,6 +7,7 @@ import {
   PopoverContent,
   PopoverTrigger,
   useBoolean,
+  useToast,
 } from "@chakra-ui/react";
 
 import useEventReactions from "../../../hooks/use-event-reactions";
@@ -19,28 +20,43 @@ import { AddReactionIcon } from "../../icons";
 import ReactionPicker from "../../reaction-picker";
 import { draftEventReaction } from "../../../helpers/nostr/reactions";
 import { getEventUID } from "../../../helpers/nostr/events";
+import { useState } from "react";
 
 export default function ReactionButton({ event, ...props }: { event: NostrEvent } & Omit<ButtonProps, "children">) {
+  const toast = useToast();
   const { requestSignature } = useSigningContext();
   const reactions = useEventReactions(getEventUID(event)) ?? [];
   const [popover, setPopover] = useBoolean();
 
+  const [loading, setLoading] = useState(false);
   const addReaction = async (emoji = "+", url?: string) => {
-    const draft = draftEventReaction(event, emoji, url);
+    setLoading(true);
+    try {
+      const draft = draftEventReaction(event, emoji, url);
 
-    const signed = await requestSignature(draft);
-    if (signed) {
-      const writeRelays = clientRelaysService.getWriteUrls();
-      new NostrPublishAction("Reaction", writeRelays, signed);
-      eventReactionsService.handleEvent(signed);
-      setPopover.off();
+      const signed = await requestSignature(draft);
+      if (signed) {
+        const writeRelays = clientRelaysService.getWriteUrls();
+        new NostrPublishAction("Reaction", writeRelays, signed);
+        eventReactionsService.handleEvent(signed);
+        setPopover.off();
+      }
+    } catch (e) {
+      if (e instanceof Error) toast({ description: e.message, status: "error" });
     }
+    setLoading(false);
   };
 
   return (
     <Popover isLazy isOpen={popover} onOpen={setPopover.on} onClose={setPopover.off}>
       <PopoverTrigger>
-        <IconButton icon={<AddReactionIcon />} aria-label="Add Reaction" {...props}>
+        <IconButton
+          icon={<AddReactionIcon />}
+          aria-label="Add Reaction"
+          title="Add Reaction"
+          isLoading={loading}
+          {...props}
+        >
           {reactions?.length ?? 0}
         </IconButton>
       </PopoverTrigger>
