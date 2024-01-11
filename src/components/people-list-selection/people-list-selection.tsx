05b4ca2f1efc35521d1352197a462674d49837b9
hzrd149 <github@hzrd149.com>
2024-01-11 11:57:32 +0000

    Add search when selecting list in feed

--- src/components/people-list-selection/people-list-selection.tsx
+++ src/components/people-list-selection/people-list-selection.tsx
@@ -1,20 +1,53 @@
 import {
   Button,
+  ButtonGroup,
   ButtonProps,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItemOption,
-  MenuList,
-  MenuOptionGroup,
+  Heading,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  SimpleGrid,
+  useDisclosure,
 } from "@chakra-ui/react";
 
 import { usePeopleListContext } from "../../providers/local/people-list-provider";
 import useUserLists from "../../hooks/use-user-lists";
 import useCurrentAccount from "../../hooks/use-current-account";
-import { PEOPLE_LIST_KIND, getListName } from "../../helpers/nostr/lists";
-import { getEventCoordinate } from "../../helpers/nostr/events";
+import { PEOPLE_LIST_KIND, getListName, getPubkeysFromList } from "../../helpers/nostr/lists";
+import { getEventCoordinate, getEventUID } from "../../helpers/nostr/events";
 import useFavoriteLists from "../../hooks/use-favorite-lists";
+import { NostrEvent } from "../../types/nostr-event";
+import { useCallback, useState } from "react";
+import useUserContactList from "../../hooks/use-user-contact-list";
+import { useUserSearchDirectoryContext } from "../../providers/global/user-directory-provider";
+import { matchSorter } from "match-sorter";
+import UserAvatar from "../user-avatar";
+import UserName from "../user-name";
+
+function ListCard({ list, ...props }: { list: NostrEvent } & Omit<ButtonProps, "children`">) {
+  return (
+    <Button justifyContent="flex-start" {...props}>
+      {getListName(list)}
+    </Button>
+  );
+}
+function PersonCard({ pubkey, ...props }: { pubkey: string } & Omit<ButtonProps, "children`">) {
+  return (
+    <Button
+      leftIcon={<UserAvatar pubkey={pubkey} size="sm" />}
+      isTruncated
+      justifyContent="flex-start"
+      p="2"
+      {...props}
+    >
+      <UserName pubkey={pubkey} />
+    </Button>
+  );
+}
 
 export default function PeopleListSelection({
   hideGlobalOption = false,
@@ -22,55 +55,111 @@ export default function PeopleListSelection({
 }: {
   hideGlobalOption?: boolean;
 } & Omit<ButtonProps, "children">) {
+  const modal = useDisclosure();
   const account = useCurrentAccount();
   const lists = useUserLists(account?.pubkey);
   const { lists: favoriteLists } = useFavoriteLists();
   const { selected, setSelected, listEvent } = usePeopleListContext();
 
-  const handleSelect = (value: string | string[]) => {
-    if (typeof value === "string") {
-      setSelected(value);
-    }
-  };
+  const getSearchDirectory = useUserSearchDirectoryContext();
+  const contacts = useUserContactList(account?.pubkey);
+  const getSearchResults = useCallback(
+    (search: string) => {
+      const pubkeys = contacts ? getPubkeysFromList(contacts).map((p) => p.pubkey) : [];
+      const filteredByContacts = getSearchDirectory().filter((p) => pubkeys.includes(p.pubkey));
+      return matchSorter(filteredByContacts, search.trim(), { keys: ["names"] }).slice(0, 10);
+    },
+    [contacts, getSearchDirectory],
+  );
+  const [search, setSearch] = useState("");
+
+  const selectList = useCallback(
+    (list: NostrEvent) => {
+      setSelected(getEventCoordinate(list));
+      modal.onClose();
+    },
+    [setSelected, modal.onClose],
+  );
+  const selectPerson = useCallback(
+    (pubkey: string) => {
+      setSelected(`3:${pubkey}`);
+      modal.onClose();
+      setSearch("");
+    },
+    [setSelected, modal.onClose, setSearch],
+  );
 
   return (
-    <Menu isLazy>
-      <MenuButton as={Button} {...props}>
+    <>
+      <Button onClick={modal.onOpen} {...props}>
         {listEvent ? getListName(listEvent) : selected === "global" ? "Global" : "Loading..."}
-      </MenuButton>
-      <MenuList zIndex={100}>
-        <MenuOptionGroup value={selected} onChange={handleSelect} type="radio">
-          {account && <MenuItemOption value="following">Following</MenuItemOption>}
-          {!hideGlobalOption && <MenuItemOption value="global">Global</MenuItemOption>}
-          {lists.length > 0 && <MenuDivider />}
-          {lists
-            .filter((l) => l.kind === PEOPLE_LIST_KIND)
-            .map((list) => (
-              <MenuItemOption key={getEventCoordinate(list)} value={getEventCoordinate(list)} isTruncated maxW="90vw">
-                {getListName(list)}
-              </MenuItemOption>
-            ))}
-        </MenuOptionGroup>
-        {favoriteLists.length > 0 && (
-          <>
-            <MenuDivider />
-            <MenuOptionGroup value={selected} onChange={handleSelect} type="radio" title="Favorites">
-              {favoriteLists
+      </Button>
+      <Modal isOpen={modal.isOpen} onClose={modal.onClose} size="2xl">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader p="4">Select List</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody px="4" pb="4" pt="0" display="flex" flexDirection="column" gap="2">
+            <ButtonGroup>
+              {account && (
+                <Button
+                  onClick={() => {
+                    setSelected("following");
+                    modal.onClose();
+                  }}
+                >
+                  Following
+                </Button>
+              )}
+              {!hideGlobalOption && (
+                <Button
+                  onClick={() => {
+                    setSelected("global");
+                    modal.onClose();
+                  }}
+                >
+                  Global
+                </Button>
+              )}
+            </ButtonGroup>
+            <Heading mt="2" size="md">
+              Lists
+            </Heading>
+            <SimpleGrid columns={2} spacing="2">
+              {lists
                 .filter((l) => l.kind === PEOPLE_LIST_KIND)
                 .map((list) => (
-                  <MenuItemOption
-                    key={getEventCoordinate(list)}
-                    value={getEventCoordinate(list)}
-                    isTruncated
-                    maxW="90vw"
-                  >
-                    {getListName(list)}
-                  </MenuItemOption>
+                  <ListCard key={getEventUID(list)} list={list} onClick={() => selectList(list)} />
                 ))}
-            </MenuOptionGroup>
-          </>
-        )}
-      </MenuList>
-    </Menu>
+            </SimpleGrid>
+            {favoriteLists.length > 0 && (
+              <>
+                <Heading mt="2" size="md">
+                  Favorites
+                </Heading>
+                <SimpleGrid columns={2} spacing="2">
+                  {favoriteLists.map((list) => (
+                    <ListCard key={getEventUID(list)} list={list} onClick={() => selectList(list)} />
+                  ))}
+                </SimpleGrid>
+              </>
+            )}
+            <Input
+              type="search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Search Contacts"
+            />
+            {search.length > 2 && (
+              <SimpleGrid columns={2} spacing="2">
+                {getSearchResults(search).map((person) => (
+                  <PersonCard key={person.pubkey} pubkey={person.pubkey} onClick={() => selectPerson(person.pubkey)} />
+                ))}
+              </SimpleGrid>
+            )}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
--- src/providers/global/user-directory-provider.tsx
+++ src/providers/global/user-directory-provider.tsx
@@ -1,51 +1,19 @@
 import { PropsWithChildren, createContext, useCallback, useContext } from "react";
+import { useAsync } from "react-use";
 
 import db from "../../services/db";
 
 export type UserDirectory = { pubkey: string; names: [] }[];
-export type GetDirectoryFn = () => Promise<UserDirectory> | UserDirectory;
-const UserSearchDirectoryContext = createContext<GetDirectoryFn>(async () => []);
+export type GetDirectoryFn = () => UserDirectory;
+const UserSearchDirectoryContext = createContext<GetDirectoryFn>(() => []);
 
 export function useUserSearchDirectoryContext() {
   return useContext(UserSearchDirectoryContext);
 }
 
-// export function getNameDirectory(directory: UserDirectory) {
-//   const people: { pubkey: string; names: string[] }[] = [];
-//   for (const pubkey of directory) {
-//     const metadata = userMetadataService.getSubject(pubkey).value;
-//     if (!metadata) continue;
-//     const names: string[] = [];
-//     if (metadata.display_name) names.push(metadata.display_name);
-//     if (metadata.name) names.push(metadata.name);
-//     if (names.length > 0) {
-//       people.push({ pubkey, names });
-//     }
-//   }
-//   return people;
-// }
-
-// export function UserContactsUserDirectoryProvider({ children, pubkey }: PropsWithChildren & { pubkey?: string }) {
-//   const account = useCurrentAccount();
-//   const contacts = useUserContactList(pubkey || account?.pubkey);
-
-//   const getDirectory = useCallback(async () => {
-//     const people = contacts ? getPubkeysFromList(contacts).map((p) => p.pubkey) : [];
-//     const directory: UserDirectory = [];
-
-//     for (const pubkey of people) {
-//       directory.push(pubkey);
-//     }
-//     return directory;
-//   }, [contacts]);
-
-//   return <UserDirectoryProvider getDirectory={getDirectory}>{children}</UserDirectoryProvider>;
-// }
-
 export function AllUserSearchDirectoryProvider({ children }: PropsWithChildren) {
-  const getDirectory = useCallback(async () => {
-    return await db.getAll("userSearch");
-  }, []);
+  const { value: users } = useAsync(() => db.getAll("userSearch"));
+  const getDirectory = useCallback(() => users as UserDirectory, [users]);
 
   return <UserSearchDirectoryProvider getDirectory={getDirectory}>{children}</UserSearchDirectoryProvider>;
 }
@@ -55,9 +23,9 @@ export default function UserSearchDirectoryProvider({
   getDirectory,
 }: PropsWithChildren & { getDirectory: GetDirectoryFn }) {
   const parent = useContext(UserSearchDirectoryContext);
-  const wrapper = useCallback<() => Promise<UserDirectory>>(async () => {
-    const dir = parent ? await parent() : [];
-    const newDir = await getDirectory();
+  const wrapper = useCallback<() => UserDirectory>(() => {
+    const dir = parent ? parent() : [];
+    const newDir = getDirectory();
     for (const pubkey of newDir) {
       if (!dir.includes(pubkey)) dir.push(pubkey);
     }
