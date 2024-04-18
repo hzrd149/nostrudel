974c7770919ef7d2a27d6c0ab502b505749e21a0
Vic <88121568+vicariousdrama@users.noreply.github.com>
2024-04-18 03:54:04 +0000

    Increase size of custom emoji reactions

--- src/components/event-reactions/reaction-icon.tsx
+++ src/components/event-reactions/reaction-icon.tsx
@@ -4,6 +4,6 @@ import { DislikeIcon, LikeIcon } from "../icons";
 export default function ReactionIcon({ emoji, url }: { emoji: string; url?: string }) {
   if (emoji === "+") return <LikeIcon />;
   if (emoji === "-") return <DislikeIcon />;
-  if (url) return <Image src={url} title={emoji} alt={emoji} w="1em" h="1em" display="inline" overflow="hidden" />;
+  if (url) return <Image src={url} title={emoji} alt={emoji} w="1.8em" h="1.8em" display="inline" overflow="hidden" />;
   return <span>{emoji}</span>;
 }
