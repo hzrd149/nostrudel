7894e85bf58ad28ea4a273838fe8ebbc0fb9102a
hzrd149 <github@hzrd149.com>
2024-05-06 09:26:14 -0500

    another fix

--- src/helpers/regexp.ts
+++ src/helpers/regexp.ts
@@ -2,7 +2,7 @@ export const getMatchNostrLink = () =>
   /(nostr:|@)?((npub|note|nprofile|nevent|nrelay|naddr)1[qpzry9x8gf2tvdw0s3jn54khce6mua7l]{58,})/gi;
 export const getMatchHashtag = () => /(^|[^\p{L}])#([\p{L}\p{N}\p{M}]+)/gu;
 export const getMatchLink = () =>
-  /https?:\/\/([a-zA-Z0-9\.\-]+\.[a-zA-Z]+)([\/\?#][\p{L}\p{N}\p{M}&\.-\/\?=#\-@%\+_,:!~*]*)/gu;
+  /https?:\/\/([a-zA-Z0-9\.\-]+\.[a-zA-Z]+)([\/\?#][\p{L}\p{N}\p{M}&\.-\/\?=#\-@%\+_,:!~*]*)?/gu;
 export const getMatchEmoji = () => /:([a-zA-Z0-9_-]+):/gi;
 export const getMatchCashu = () => /(cashuA[A-Za-z0-9_-]{0,10000}={0,3})/gi;
 export const getMatchSimpleEmail = () => /^[^\s]{1,64}@[^\s]+\.[^\s]{2,}$/;
