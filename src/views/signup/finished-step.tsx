8b03e15b51665eb195549d3456d7c9549a81f483
Logen <79722764+btcforplebs@users.noreply.github.com>
2024-12-02 21:30:06 -0500

    Update finished-step.tsx

--- src/views/signup/finished-step.tsx
+++ src/views/signup/finished-step.tsx
@@ -37,22 +37,6 @@ export default function FinishedStep() {
 
   return (
     <Flex gap="4" {...containerProps} maxW="6in">
-      <Heading>Follow a few others</Heading>
-      <Flex overflowX="hidden" overflowY="scroll" minH="4in" maxH="6in" direction="column" gap="2" w="full">
-        {trending?.profiles.map(({ pubkey, profile }) => (
-          <Card p="4" key={pubkey} variant="outline" gap="2">
-            <Flex direction="row" alignItems="center" gap="4">
-              <UserAvatarLink pubkey={pubkey} />
-              <Flex direction="column" overflow="hidden">
-                <UserLink pubkey={pubkey} fontWeight="bold" fontSize="lg" isTruncated />
-                <UserDnsIdentity pubkey={pubkey} />
-              </Flex>
-              <UserFollowButton pubkey={pubkey} flexShrink={0} ml="auto" />
-            </Flex>
-            {profile && <About profile={profile} />}
-          </Card>
-        ))}
-      </Flex>
       <Button as={RouterLink} to="/" colorScheme="primary" maxW="sm" w="full">
         Start exploring nostr
       </Button>
