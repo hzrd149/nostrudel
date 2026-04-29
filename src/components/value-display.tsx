c0121dffb88c3868ce2867958d2d147df6351d6c
hzrd149 <github@hzrd149.com>
2026-04-29 11:38:44 -0500

    Display native sats without unit

--- src/components/value-display.tsx
+++ src/components/value-display.tsx
@@ -2,7 +2,7 @@ import { Text, TextProps } from "@chakra-ui/react";
 import { use$ } from "applesauce-react/hooks";
 import { useEffect } from "react";
 
-import { formatSats, formatSatsAsCurrency } from "../helpers/lightning";
+import { formatSatsAsCurrency, humanReadableSats } from "../helpers/lightning";
 import { exchangeRates$, refreshExchangeRates } from "../services/exchange-rates";
 import localSettings from "../services/preferences";
 
@@ -20,7 +20,7 @@ export default function ValueDisplay({ sats, title, ...props }: ValueDisplayProp
 
   const currency = displayCurrency?.toUpperCase() ?? null;
   const rate = currency ? exchangeRates?.rates[currency] : undefined;
-  const fallback = formatSats(sats);
+  const fallback = humanReadableSats(sats);
   const value = currency && rate ? formatSatsAsCurrency(sats, currency, rate) : fallback;
 
   return (
